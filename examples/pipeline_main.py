b63b1802f1bfa8718e8fd10dc643a580613700a0
akatief <f.tib29@gmail.com>
2022-06-07 01:47:43 +0200

    feat: implement FeverousRetrieverEntropy and relative methods

--- examples/pipeline_main.py
+++ examples/pipeline_main.py
@@ -9,12 +9,13 @@ from src.claim import FeverousGenerator, ToTToGenerator
 
 from src.pipeline import ClaimGeneratorPipeline
 from src.evidence.feverous_retriever.random import FeverousRetrieverRandom
+from src.evidence.feverous_retriever.entropy import FeverousRetrieverEntropy
 
 
 
 @hydra.main(config_path="../src/config/", config_name="config_pipeline.yaml")
 def main(cfg):
-    retrievers = [FeverousRetrieverRandom(p_dataset=cfg.main.data_path,
+    retrievers = [FeverousRetrieverEntropy(p_dataset=cfg.main.data_path,
                                           num_positive=cfg.positive_evidence,
                                           num_negative=cfg.negative_evidence,
                                           wrong_cell=cfg.wrong_cell,
@@ -22,24 +23,23 @@ def main(cfg):
                                           evidence_per_table=cfg.evidence_per_table,
                                           column_per_table=cfg.column_per_table,
                                           seed=cfg.seed,
-                                          key_strategy=strat,
                                           verbose=True
                                           )
                   # for strat in ['entity', 'random']
                   for strat in ['entity']
                   ]
 
-    generator1 = FeverousGenerator(encoding='totto',
+    generator1 = FeverousGenerator(encoding='compact',
                                    model_path=cfg.main.model_path,)
 
     generator2 = ToTToGenerator(
-    encoding='totto', model_path='../models/exported_totto_large/1648208035'
+    encoding='totto', model_path='../../../../models/exported_totto_large/1648208035'
     )
-    generator3 = ToTToGenerator(encoding='compact',
-                                model_path=cfg.main.model_path,
-                                verbose=True)
-    generator = ToTToGenerator(encoding='totto',
-                               model_path=cfg.main.model_path)
+    # generator3 = ToTToGenerator(encoding='compact',
+    #                             model_path=cfg.main.model_path,
+    #                             verbose=True)
+    # generator = ToTToGenerator(encoding='totto',
+    #                            model_path=cfg.main.model_path)
 
     generators = [generator2]
 
